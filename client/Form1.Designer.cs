903b2a33b2c24fcc9fb90fa25b2cc4cc184fd9a1
yagiz827 <yagiztufek@sabanciuniv.edu>
2022-12-12 21:18:50 +0300

    Add project files.

---
+++ client/Form1.Designer.cs
@@ -0,0 +1,195 @@
+﻿namespace client
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox_ip = new System.Windows.Forms.TextBox();
+            this.textBox_port = new System.Windows.Forms.TextBox();
+            this.button_connect = new System.Windows.Forms.Button();
+            this.logs = new System.Windows.Forms.RichTextBox();
+            this.textBox_message = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button_send = new System.Windows.Forms.Button();
+            this.textBox_name = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(45, 52);
+            this.label1.Margin = new System.Windows.Forms.Padding(1, 0, 1, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(20, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "IP:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(35, 79);
+            this.label2.Margin = new System.Windows.Forms.Padding(1, 0, 1, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(29, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Port:";
+            // 
+            // textBox_ip
+            // 
+            this.textBox_ip.Location = new System.Drawing.Point(67, 51);
+            this.textBox_ip.Margin = new System.Windows.Forms.Padding(1);
+            this.textBox_ip.Name = "textBox_ip";
+            this.textBox_ip.Size = new System.Drawing.Size(88, 20);
+            this.textBox_ip.TabIndex = 2;
+            this.textBox_ip.TextChanged += new System.EventHandler(this.textBox_ip_TextChanged);
+            // 
+            // textBox_port
+            // 
+            this.textBox_port.Location = new System.Drawing.Point(67, 79);
+            this.textBox_port.Margin = new System.Windows.Forms.Padding(1);
+            this.textBox_port.Name = "textBox_port";
+            this.textBox_port.Size = new System.Drawing.Size(88, 20);
+            this.textBox_port.TabIndex = 3;
+            this.textBox_port.TextChanged += new System.EventHandler(this.textBox_port_TextChanged);
+            // 
+            // button_connect
+            // 
+            this.button_connect.Location = new System.Drawing.Point(67, 112);
+            this.button_connect.Margin = new System.Windows.Forms.Padding(1);
+            this.button_connect.Name = "button_connect";
+            this.button_connect.Size = new System.Drawing.Size(69, 22);
+            this.button_connect.TabIndex = 4;
+            this.button_connect.Text = "connect";
+            this.button_connect.UseVisualStyleBackColor = true;
+            this.button_connect.Click += new System.EventHandler(this.button_connect_Click);
+            // 
+            // logs
+            // 
+            this.logs.Location = new System.Drawing.Point(243, 52);
+            this.logs.Margin = new System.Windows.Forms.Padding(1);
+            this.logs.Name = "logs";
+            this.logs.Size = new System.Drawing.Size(192, 261);
+            this.logs.TabIndex = 5;
+            this.logs.Text = "";
+            // 
+            // textBox_message
+            // 
+            this.textBox_message.Enabled = false;
+            this.textBox_message.Location = new System.Drawing.Point(67, 274);
+            this.textBox_message.Margin = new System.Windows.Forms.Padding(1);
+            this.textBox_message.Name = "textBox_message";
+            this.textBox_message.Size = new System.Drawing.Size(98, 20);
+            this.textBox_message.TabIndex = 6;
+            this.textBox_message.TextChanged += new System.EventHandler(this.textBox_message_TextChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(10, 276);
+            this.label3.Margin = new System.Windows.Forms.Padding(1, 0, 1, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(53, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Message:";
+            // 
+            // button_send
+            // 
+            this.button_send.Enabled = false;
+            this.button_send.Location = new System.Drawing.Point(166, 269);
+            this.button_send.Margin = new System.Windows.Forms.Padding(1);
+            this.button_send.Name = "button_send";
+            this.button_send.Size = new System.Drawing.Size(65, 26);
+            this.button_send.TabIndex = 8;
+            this.button_send.Text = "send";
+            this.button_send.UseVisualStyleBackColor = true;
+            this.button_send.Click += new System.EventHandler(this.button_send_Click);
+            // 
+            // textBox_name
+            // 
+            this.textBox_name.BackColor = System.Drawing.Color.White;
+            this.textBox_name.Location = new System.Drawing.Point(67, 145);
+            this.textBox_name.Margin = new System.Windows.Forms.Padding(1);
+            this.textBox_name.Name = "textBox_name";
+            this.textBox_name.Size = new System.Drawing.Size(88, 20);
+            this.textBox_name.TabIndex = 9;
+            this.textBox_name.TextChanged += new System.EventHandler(this.textBox_name_TextChanged);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(34, 147);
+            this.label4.Margin = new System.Windows.Forms.Padding(1, 0, 1, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(35, 13);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Name";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(450, 358);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox_name);
+            this.Controls.Add(this.button_send);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox_message);
+            this.Controls.Add(this.logs);
+            this.Controls.Add(this.button_connect);
+            this.Controls.Add(this.textBox_port);
+            this.Controls.Add(this.textBox_ip);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(1);
+            this.Name = "Form1";
+            this.Text = "Client";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox_ip;
+        private System.Windows.Forms.TextBox textBox_port;
+        private System.Windows.Forms.Button button_connect;
+        private System.Windows.Forms.RichTextBox logs;
+        private System.Windows.Forms.TextBox textBox_message;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button_send;
+        private System.Windows.Forms.TextBox textBox_name;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ client/Form1.cs
@@ -0,0 +1,217 @@
+﻿using System;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace client
+{
+    public partial class Form1 : Form
+    {
+
+        bool terminating = false;
+        bool connected = false;
+        Socket clientSocket;
+
+        public Form1()
+        {
+            Control.CheckForIllegalCrossThreadCalls = false;
+            this.FormClosing += new FormClosingEventHandler(Form1_FormClosing);
+
+            InitializeComponent();
+
+            button_send.Enabled = false;
+
+            textBox_ip.Text = "localhost";
+            textBox_port.Text = "5000";
+            textBox_name.Text = "User1";
+        }
+
+        private void button_connect_Click(object sender, EventArgs e)
+        {
+            clientSocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            string IP = textBox_ip.Text;
+
+            int portNum;
+            if (Int32.TryParse(textBox_port.Text, out portNum))
+            {
+                try
+                {
+                    logs.Text = "";
+                    clientSocket.Connect(IP, portNum);
+                    button_connect.Enabled = false;
+                    textBox_message.Enabled = true;
+                    connected = true;
+                    logs.AppendText("Connected to the server!\n");
+                    string name = textBox_name.Text;
+                    if (name != "" && name.Length <= 64)
+                    {
+                        Thread.Sleep(100);
+                        var helloMessage = "Name," + name;
+                        Byte[] buffer = Encoding.Default.GetBytes(helloMessage);
+                        clientSocket.Send(buffer);
+                    }
+
+                    Thread receiveThread = new Thread(Receive);
+                    receiveThread.Start();
+
+                }
+                catch
+                {
+                    logs.AppendText("Could not connect to the server!\n");
+                }
+            }
+            else
+            {
+                logs.AppendText("Check the port\n");
+            }
+
+        }
+
+        private void Receive()
+        {
+            while (IsSocketConnected(clientSocket) && connected)
+            {
+                try
+                {
+                    if (clientSocket.Available > 0)
+                    {
+                        Byte[] buffer = new Byte[1000]; // serverdan alma fonksiyonu bu 64 bitle kısıtlı
+                        clientSocket.Receive(buffer);
+
+                        string incomingMessage = Encoding.Default.GetString(buffer);
+                        incomingMessage = incomingMessage.Substring(0, incomingMessage.IndexOf("\0"));
+
+                        logs.AppendText("Server: " + incomingMessage + "\n");
+
+                        if (incomingMessage.Contains(" has won!") || incomingMessage.Contains("Your name is already used. "))
+                        {
+                            ResetClient();
+                        }
+                    }
+                }
+                catch
+                {
+                    if (!terminating)
+                    {
+                        ResetClient();
+                        logs.AppendText("The server has disconnected\n");
+                    }
+                }
+
+            }
+            logs.AppendText("Server disconnected");
+            ResetClient();
+
+
+        }
+
+
+        private bool IsSocketConnected(Socket socket)
+        {
+            try
+            {
+                if (!socket.Connected)
+                    return false;
+
+                bool part1 = socket.Poll(1000, SelectMode.SelectRead);
+                bool part2 = (socket.Available == 0);
+                return !(part1 && part2);
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+
+        private void ResetClient()
+        {
+            connected = false;
+            clientSocket.Close();
+            textBox_ip.Text = "localhost";
+            textBox_port.Text = "5000";
+            textBox_name.Text = "User1";
+            button_send.Enabled = false;
+            button_connect.Enabled = true;
+            textBox_message.Enabled = false;
+            textBox_message.Text = "";
+        }
+
+        private void Form1_FormClosing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            connected = false;
+            terminating = true;
+            if (clientSocket != null)
+                clientSocket.Close();
+            Environment.Exit(0);
+        }
+
+        private void button_send_Click(object sender, EventArgs e)
+        {
+            string answer = textBox_message.Text;
+
+            if (answer != "" && answer.Length <= 64)
+            {
+                var message = "Answer," + answer;
+                Byte[] buffer = Encoding.UTF8.GetBytes(message);
+                clientSocket.Send(buffer);
+            }
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox_name_TextChanged(object sender, EventArgs e)
+        {
+            HandleConnectButtonOnInputChanged();
+        }
+
+        private void textBox_message_TextChanged(object sender, EventArgs e)
+        {
+            var isInputNumber = int.TryParse(textBox_message.Text, out var number);
+            if (textBox_message.Text == String.Empty || !isInputNumber)
+            {
+                button_send.Enabled = false;
+            }
+            else
+            {
+                button_send.Enabled = true;
+            }
+        }
+
+        private void textBox_ip_TextChanged(object sender, EventArgs e)
+        {
+
+            HandleConnectButtonOnInputChanged();
+        }
+
+        private void textBox_port_TextChanged(object sender, EventArgs e)
+        {
+            HandleConnectButtonOnInputChanged();
+        }
+
+        private void HandleConnectButtonOnInputChanged()
+        {
+            var isIpOk = textBox_ip.Text != String.Empty;
+            var isPortOk = textBox_port.Text != String.Empty && int.TryParse(textBox_port.Text, out var port);
+            var isNameOk = textBox_name.Text != String.Empty;
+            if (isIpOk && isPortOk && isNameOk)
+            {
+                button_connect.Enabled = true;
+            }
+            else
+            {
+                button_connect.Enabled = false;
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
